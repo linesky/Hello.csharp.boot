0cea5c3dfd26510dbc170e0b35d890c317f2bc6a
sky line <32512284+linesky@users.noreply.github.com>
2024-05-31 21:37:51 +0100

    Add files via upload

--- shell.py
+++ shell.py
@@ -5,32 +5,48 @@ from tkinter.filedialog import asksaveasfilename
 import subprocess
 import shutil
 import os
-import sys
 
 
 
 class BareboneBuilder:
     def __init__(self, root):
         self.root = root
-        self.root.title("run")
+        self.root.title("gzip")
 
         # Janela amarela
         self.root.configure(bg='yellow')
 
-        
+        # Área de texto
+        self.text_area = tk.Text(self.root, height=10, width=50)
+        self.text_area.pack(pady=10)
 
         # Botões
-        self.build_button = tk.Button(self.root, text="run", command=self.build_kernel)
+        self.build_button = tk.Button(self.root, text="Build", command=self.build_kernel)
         self.build_button.pack(pady=5)
 
         
-
-    
+    def execute_command(self, command,show:bool):
+        try:
+            
+            result = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True, text=True)
+            self.text_area.insert(tk.END, result)
+        except subprocess.CalledProcessError as e:
+            if show:
+                self.text_area.insert(tk.END,f"Error executing command:\n{e.output}")
 
     def build_kernel(self):
+        filename = tk.filedialog.askopenfilename(title="Select file")
+        self.text_area.delete(1.0, tk.END)
         
-        os.system('pkexec nautilus --browser ./')
+        self.text_area.insert(tk.END,"build ... "+filename)
+        self.execute_command('cp $filename $filename.bak'.replace("$filename",filename),True)
+        self.execute_command('gzip $filename'.replace("$filename",filename),True)
         
+        
+    
+            
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     builder = BareboneBuilder(root)
